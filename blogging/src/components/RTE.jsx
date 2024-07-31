f55a5f9ea40a4d8a7183ff0df9dfe94ea7fd3869
Poonamraut040 <poonam25ec048@satiengg.in>
2024-07-31 17:16:44 +0530

    commit

---
+++ blogging/src/components/RTE.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import {Controller } from 'react-hook-form';
+
+
+export default function RTE({name, control, label, defaultValue =""}) {
+  return (
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+    <Controller
+    name={name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+        <Editor
+        apiKey='g1bl44mw5taj0y6rr60vl3lmuj3oulbhhh3xz7zzd57bgp6z'
+        initialValue={defaultValue}
+        init={{
+            initialValue: defaultValue,
+            height: 500,
+            menubar: true,
+            plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+            ],
+            toolbar:
+            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        }}
+        onEditorChange={onChange}
+        />
+    )}
+    />
+
+     </div>
+  )
+}
